0fb4c41734c4f0822310c1a91b2e74dcab27aa50
EC2 Default User <ec2-user@ip-172-31-2-217.ec2.internal>
2021-12-07 03:29:23 +0000

    add flash deptroy edit success

--- app/controllers/bookmakers_controller.rb
+++ app/controllers/bookmakers_controller.rb
@@ -7,14 +7,14 @@ class BookmakersController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "SUCCESS"
-      redirect_to bookmaker_path
+      redirect_to bookmaker_path(@book.id)
     else
       flash[:alert] = "ERROR"
       redirect_to bookmakers_path
     end
   end
-  def index
 
+  def index
     @books = Book.all
     @books = Book.all.order(created_at: :desc)
   end
@@ -28,6 +28,7 @@ class BookmakersController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to edit_bookmaker_path
     else
       render 'bookmakers/edit'
@@ -36,6 +37,7 @@ class BookmakersController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed"
     redirect_to bookmakers_path
   end
 
